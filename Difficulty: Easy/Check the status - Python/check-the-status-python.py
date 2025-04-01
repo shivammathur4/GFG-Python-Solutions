e26f392b892041465962939731006d152776660b
shivammathur4 <35236428+shivammathur4@users.noreply.github.com>
2025-04-02 02:02:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check the status - Python/check-the-status-python.py
@@ -0,0 +1,45 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+#User function Template for python3
+class Solution:
+    def checkStatus(self, a, b, flag):
+        return ((a >= 0) ^ (b >= 0)) and not flag or (a < 0 and b < 0 and flag)
+
+
+
+
+#{ 
+ # Driver Code Starts.
+
+# Driver Code
+def main():
+    
+    # Testcase input
+    testcases = int(input())
+    
+    # Looping through testcases
+    while(testcases > 0):
+        a = int(input())
+        b = int(input())
+        flag = input()
+        
+        if(flag == "True"):
+            flag = True
+        else:
+            flag = False
+        
+        if(Solution().checkStatus(a, b, flag) is True):
+            print ("True")
+        else:
+            print ("False")
+        
+        testcases -= 1
+        print("~")
+ 
+if __name__ == '__main__':
+    main()
+# } Driver Code Ends
